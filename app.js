bbb1bf77b90a8c54ea136f779c9714cd35617f5d
mohammedboreny <Mohammed.boreny@gmail.com>
2022-08-10 16:44:11 +0300

    more Adjustments

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 let Name=prompt("Enter your name");
 let gender="";
 gender = prompt("Enter your gender");
-if (gender != "Male" && gender != "Female")
-alert("please provide your gender as male or female");
-
-
-age=prompt("Enter your age")
-if (age <= 0) 
-alert("Age is less than 0");
-    
-y = confirm("Do you want to continue?")
-if (y == true) {
-    if (gender == "Male")
-        alert("Welcome Mr" + Name);
-    else
-        alert("Welcome Ms" + Name);
+if (gender != "Male" && gender != "Female") {
+    alert("please provide your gender as male or female");
 }
+else {
+    age = prompt("Enter your age")
+    if (age <= 0) {
+        alert("Age is less than 0");
+    }
+    else {
+        y = confirm("Do you want to continue?")
+        if (y == true) {
+            if (gender == "Male")
+                alert("Welcome Mr" + Name);
+        }
+        else {
+            alert("Welcome Ms" + Name);
+        }
+        }
+    }
+    
 

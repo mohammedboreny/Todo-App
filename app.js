87847c20222d5832eb3c1660958e99ecb284398f
mohammedboreny <Mohammed.boreny@gmail.com>
2022-08-11 15:39:40 +0300

    more Adjustments

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ else {
         alert("Age is less than 0");
     }
     else {
-        y = confirm("Do you want to continue?")
+        y = confirm("Do you want to continue?");
         if (y == true) {
             if (gender == "Male")
                 alert("Welcome Mr" + Name);
@@ -18,7 +18,36 @@ else {
         else {
             alert("Welcome Ms" + Name);
         }
-        }
+    }
+    car = prompt("Do you have a car?");
+    if (car ==null) {
+        car = "invalid";
+    }
+    married = prompt("are you married?");
+    if (married ==null) {
+        married = "invalid";
+    }
+    children = prompt("Do you have a children?");
+    if (children ==null) {
+        children = "invalid";
+    }
+    let listOfAnswers = [];
+    listOfAnswers.push(Name);
+    listOfAnswers.push(gender);
+    listOfAnswers.push(age);
+    listOfAnswers.push(car);
+    listOfAnswers.push(married);
+    listOfAnswers.push(children);
+    listOfAnswersRev = listOfAnswers.reverse();
+    console.log(listOfAnswersRev);
+    ratingUser = prompt("rate our website");
+    for (i = 1; i <= ratingUser; i++){
+        let x = '';
+        x += "*";
+        if (x.length >= 4)
+            x = "*****";
+    }
+    alert("thank you for " + x + "stars");
     }
     
 
